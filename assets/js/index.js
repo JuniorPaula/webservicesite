889d0346df4a2eb75a5e6fb3eaa249f315bf811c
Junior Paula <slucivaldo509@gmail.com>
2021-10-30 10:01:03 -0300

    Created about section

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,6 +8,15 @@ $(document).ready(function() {
     $(window).on('load scroll', function() {
         $('.fa-bars').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
+
+        if($(window).scrollTop() > 30) {
+            $('.header').css({'background':'#7167c1', 'box-shadow': '0 .2rem .5rem rgba(0,0,0,.4)'});
+        
+        } else {
+            $('.header').css({'background':'none', 'box-shadow': 'none'});
+
+        }
+
     });
 
 });
