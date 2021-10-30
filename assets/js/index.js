677a10f0a67a0a8d5556ffb403ddff0d5ade2ddc
Junior Paula <slucivaldo509@gmail.com>
2021-10-30 14:03:53 -0300

    created FAQ section

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,4 +19,13 @@ $(document).ready(function() {
 
     });
 
+    $('.accordion-header').click(function() {
+        $('.accordion .accordion-body').slideUp();
+        $(this).next('.accordion-body').slideDown();
+        $('.accordion .accordion-header span').text('+');
+        $(this).children('span').text('-');
+
+
+    });
+
 });
